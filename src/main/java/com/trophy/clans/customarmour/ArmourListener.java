04c9f27dbb4e4592b5e18c004ea2620a5916183b
BuildTools <unconfigured@null.spigotmc.org>
2021-01-13 00:12:32 +0100

    Cleaned up most classes

--- src/main/java/com/trophy/clans/customarmour/ArmourListener.java
+++ src/main/java/com/trophy/clans/customarmour/ArmourListener.java
@@ -4,13 +4,11 @@ import com.trophy.clans.main.Clans;
 import com.trophy.clans.utility.Items;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class ArmourListener implements Listener {
+public class ArmourListener {
 
 	private final Clans plugin;
 	private final Items items;
@@ -18,97 +16,56 @@ public class ArmourListener implements Listener {
 	public ArmourListener(final Items items, final Clans plugin) {
 		this.items = items;
 		this.plugin = plugin;
+		startTask();
 	}
 
-	@EventHandler
-	public void startTask() {
-		Bukkit.getScheduler().runTaskTimerAsynchronously(plugin, () -> {
-			for (final Player p : Bukkit.getServer().getOnlinePlayers()) {
-				final ItemStack helmet = p.getInventory().getHelmet();
-				final ItemStack chest = p.getInventory().getChestplate();
-				final ItemStack legs = p.getInventory().getLeggings();
-				final ItemStack boots = p.getInventory().getBoots();
+	private void startTask() {
+		Bukkit.getScheduler().runTaskTimer(plugin, () -> {
+			for (final Player player : Bukkit.getServer().getOnlinePlayers()) {
+				final ItemStack helmet = player.getInventory().getHelmet();
+				final ItemStack chest = player.getInventory().getChestplate();
+				final ItemStack legs = player.getInventory().getLeggings();
+				final ItemStack boots = player.getInventory().getBoots();
 				if (helmet.equals(items.getTier1Helmet())) {
-					p.sendMessage("tier 1 helmet true");
-					if (checkFullTierOneArmour(p)) {
-						Bukkit.getScheduler().runTaskLater(plugin, () -> p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1)), 1L);
+					if (checkFullTierOneArmour(chest, legs, boots)) {
+						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
 					} else {
-						if (p.hasPotionEffect(PotionEffectType.SPEED)) {
-							Bukkit.getScheduler().runTaskLater(plugin, () -> p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1)), 1L);
+						if (player.hasPotionEffect(PotionEffectType.SPEED)) {
+							player.removePotionEffect(PotionEffectType.SPEED);
 						}
 					}
 				} else if (helmet.equals(items.getTier2Helmet())) {
-					p.sendMessage("tier 2 helmet true");
-					if (checkFullTierTwoArmour(p)) {
-						Bukkit.getScheduler().runTaskLater(plugin, () -> p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1)), 1L);
+					if (checkFullTierTwoArmour(chest, legs, boots)) {
+						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
 					} else {
-						Bukkit.getScheduler().runTaskLater(plugin, () -> {
-							if (p.hasPotionEffect(PotionEffectType.SPEED)) {
-								p.removePotionEffect(PotionEffectType.SPEED);
-							}
-						}, 1L);
-
+						if (player.hasPotionEffect(PotionEffectType.SPEED)) {
+							player.removePotionEffect(PotionEffectType.SPEED);
+						}
 					}
 				} else if (helmet.equals(items.getTier3Helmet())) {
-					p.sendMessage("tier 3 helmet true");
-					if (checkFullTierThreeArmour(p)) {
-						Bukkit.getScheduler().runTaskLater(plugin, () -> p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1)), 1L);
+					if (checkFullTierThreeArmour(chest, legs, boots)) {
+						player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 1));
 					} else {
-						if (p.hasPotionEffect(PotionEffectType.SPEED)) {
-							Bukkit.getScheduler().runTaskLater(plugin, () -> {
-								if (p.hasPotionEffect(PotionEffectType.SPEED)) {
-									p.removePotionEffect(PotionEffectType.SPEED);
-								}
-							}, 1L);
+						if (player.hasPotionEffect(PotionEffectType.SPEED)) {
+							player.removePotionEffect(PotionEffectType.SPEED);
 						}
 					}
-				} else {
-					Bukkit.getScheduler().runTaskLater(plugin, () -> {
-						if (p.hasPotionEffect(PotionEffectType.SPEED)) {
-							p.removePotionEffect(PotionEffectType.SPEED);
-						}
-					}, 1L);
 				}
 			}
-		}, 0L, 2L);
+		}, 0L, 20L);
 	}
 
 
-	private static boolean checkFullTierOneArmour(final Player player) {
-		final ItemStack chest = player.getInventory().getChestplate();
-		final ItemStack legs = player.getInventory().getLeggings();
-		final ItemStack boots = player.getInventory().getBoots();
-
-		if (chest.equals(items.getTierOneChestplate()) && legs.equals(items.getTierOneLeggings()) && boots.equals(items.getTierOneBoots())) {
-			return true;
-		} else {
-			return false;
-		}
+	private boolean checkFullTierOneArmour(final ItemStack chest, final ItemStack legs, final ItemStack boots) {
+		return chest.equals(items.getTier1Chestplate()) && legs.equals(items.getTier1Leggings()) && boots.equals(items.getTier1Boots());
 	}
 
-	private static boolean checkFullTierTwoArmour(final Player player) {
-		final ItemStack chest = player.getInventory().getChestplate();
-		final ItemStack legs = player.getInventory().getLeggings();
-		final ItemStack boots = player.getInventory().getBoots();
-
-		if (chest.equals(items.getTierTwoChestplate()) && legs.equals(items.getTierTwoLeggings()) && boots.equals(items.getTierTwoBoots())) {
-			return true;
-		} else {
-			return false;
-		}
-
+	private boolean checkFullTierTwoArmour(final ItemStack chest, final ItemStack legs, final ItemStack boots) {
+		return chest.equals(items.getTier2Chestplate()) && legs.equals(items.getTier2Leggings()) && boots.equals(items.getTier2Boots());
 	}
 
-	private static boolean checkFullTierThreeArmour(final Player player) {
-		final ItemStack chest = player.getInventory().getChestplate();
-		final ItemStack legs = player.getInventory().getLeggings();
-		final ItemStack boots = player.getInventory().getBoots();
-
-		if (chest.equals(items.getTierThreeChestplate()) && legs.equals(items.getTierThreeLeggings()) && boots.equals(items.getTierThreeBoots())) {
-			return true;
-		} else {
-			return false;
-		}
+	private boolean checkFullTierThreeArmour(final ItemStack chest, final ItemStack legs, final ItemStack boots) {
+		return chest.equals(items.getTier3Chestplate()) && legs.equals(items.getTier3Leggings()) && boots.equals(items.getTier3Boots());
 	}
 
 }
