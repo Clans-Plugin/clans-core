4b05d05c56b31ab33b94e8a5ec64d267ff3a38bb
benkeakos <50333278+benkeakos@users.noreply.github.com>
2021-02-10 18:45:38 +0100

    Added reconnect() which attempts to prevent SQL timeout exception

--- src/main/java/com/akosg/clans/main/Clans.java
+++ src/main/java/com/akosg/clans/main/Clans.java
@@ -9,7 +9,7 @@ import com.akosg.clans.craftingsystem.CraftingTaskHandler;
 import com.akosg.clans.customarmour.ArmourListener;
 import com.akosg.clans.customore.ResourcesListener;
 import com.akosg.clans.database.CacheManager;
-import com.akosg.clans.database.FirstJoinListener;
+import com.akosg.clans.database.PlayerCache;
 import com.akosg.clans.database.SQLInstance;
 import com.akosg.clans.levelingsystem.LevelingCommands;
 import com.akosg.clans.lootbarrels.LootListener;
@@ -17,14 +17,20 @@ import com.akosg.clans.utility.Items;
 import com.akosg.clans.utility.MenuListener;
 import org.bukkit.Bukkit;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class Clans extends JavaPlugin {
 
 private final Items items = new Items();
+
+private final HashMap<Player, PlayerCache> playerData = new HashMap<>();
 
 private CraftingTaskHandler taskHandler;
 ConsoleCommandSender console = Bukkit.getConsoleSender();
@@ -55,23 +61,30 @@ public void onDisable() {
 }
 
 
-
 public void reconnect() {
    Bukkit.getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
 	  @Override
 	  public void run() {
 		 try {
-			SQLInstance.getConnection().prepareStatement("SELECT 'Hello world'\n" +
-																	 "  FROM DUAL");
+			final PreparedStatement ps = SQLInstance.getConnection().prepareStatement("SELECT * FROM PlayerData");
+			final ResultSet rs = ps.executeQuery();
+			int total = 0;
+
+			while (rs.next()) {
+
+			   total++;
+
+			}
+
 
 			console.sendMessage("\247c[\2476Clans\247c] \247bDatabase connection refreshed!");
+			console.sendMessage("\247c[\2476Clans\247c] \247bNumber of unique players in database: " + total);
 		 } catch (final SQLException e) {
 			e.printStackTrace();
 		 }
 	  }
-   }, (30 * 60 * 20L), 10);
+   }, (30 * 60 * 20L), (30 * 60 * 20L));
 }
-
 
 private void registerTasks() {
    new ArmourListener(items, this);
@@ -93,9 +106,8 @@ private void registerListeners() {
    plm.registerEvents(new ExplosiveListener(), this);
    plm.registerEvents(new LootListener(), this);
    plm.registerEvents(new MenuListener(), this);
-   plm.registerEvents(new FirstJoinListener(), this);
    plm.registerEvents(new ChatListener(), this);
-   plm.registerEvents(new CacheManager(), this);
+   plm.registerEvents(new CacheManager(playerData), this);
 }
 
 
